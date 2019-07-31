ce94ad8309078de816c62f706f4af87500cbe749
Sajib Biswas <sajibcse28@gmail.com>
2019-07-31 15:32:53 +0600

    wriiten some looping code and created a file name is function.js

---
+++ loop.js
@@ -0,0 +1,19 @@
+var i=0;
+for(i = 0;i<10;i++){
+    console.log(i); 
+}
+
+var i=0;
+while(i<15){
+    console.log(i);   
+i++;
+}
+
+var i =0;
+do{
+    console.log(i+" ");
+    i++;
+}while(i<20);
+
+
+
